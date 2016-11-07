4a5af3ccaa560e3fe8096b4f53bb089fbc3dd63e
LDurett <ldurett@its.jnj.com>
2016-11-07 11:44:02 -0500

    Add files via upload

---
+++ str_ex1.py
@@ -0,0 +1,9 @@
+son = 'Luke'
+daughter = 'Julie'
+daughter1 = "Allie"
+whoru = raw_input("What is your name?")
+
+print "{:>25}".format(son)
+print "{:>25}".format(daughter)
+print "{:>25}".format(daughter1)
+print (whoru)
